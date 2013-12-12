692206e9896129785de7511736749d26a4a857f9
Vaclav Malek <vaclav.malek@pirati.cz>
2013-12-12 07:55:19 +0100

    add edit area render method

--- helper.php
+++ helper.php
@@ -108,6 +108,126 @@ class helper_plugin_piratihelper extends DokuWiki_Plugin {
           }
      }
 
+     public function renderEditTextarea($_text = null){
+          global $ID;
+          global $REV;
+          global $DATE;
+          global $PRE;
+          global $SUF;
+          global $INFO;
+          //global $SUM;
+          global $lang;
+          global $conf;
+          global $TEXT;
+          global $RANGE;
+
+          // added
+          $TEXT = (is_null($_text)?$TEXT:$_text);
+
+          if (isset($_REQUEST['changecheck'])) {
+               $check = $_REQUEST['changecheck'];
+          } elseif(!$INFO['exists']){
+               // $TEXT has been loaded from page template
+               $check = md5('');
+          } else {
+               $check = md5($TEXT);
+          }
+          $mod = md5($TEXT) !== $check;
+
+     //     $wr = $INFO['writable'] && !$INFO['locked'];
+          $wr=true;
+          $include = 'edit';
+          if($wr){
+               if ($REV) $include = 'editrev';
+          }else{
+               // check pseudo action 'source'
+               if(!actionOK('source')){
+                    msg('Command disabled: source',-1);
+                    return;
+               }
+               //$include = 'read';
+               $include = 'edit';
+          }
+
+          global $license;
+                                                       //
+          $form = new Doku_Form(array('id' => 'dw__editform'));
+          $form->addHidden('id', $ID);
+          $form->addHidden('rev', $REV);
+          $form->addHidden('date', $DATE);
+          $form->addHidden('prefix', $PRE . '.');
+          $form->addHidden('suffix', $SUF);
+          $form->addHidden('changecheck', $check);
+                                                       //
+          $data = array('form' => $form,
+               'wr'   => $wr,
+               'media_manager' => true,
+               'target' => (isset($_REQUEST['target']) && $wr && $RANGE !== '') ? $_REQUEST['target'] : 'section',
+               'intro_locale' => $include);
+
+          ob_start();
+
+          if ($data['target'] !== 'section') {
+               // Only emit event if page is writable, section edit data is valid and
+               // edit target is not section.
+               trigger_event('HTML_EDIT_FORMSELECTION', $data, 'html_edit_form', true);
+          } else {
+               html_edit_form($data);
+          }
+          
+          //if (isset($data['intro_locale'])) {
+          //     echo p_locale_xhtml($data['intro_locale']);
+          //}
+                                                       //
+          $form->addHidden('target', $data['target']);
+          $form->addElement(form_makeOpenTag('div', array('id'=>'wiki__editbar')));
+          $form->addElement(form_makeOpenTag('div', array('id'=>'size__ctl')));
+          $form->addElement(form_makeCloseTag('div'));
+
+          
+          //if ($wr) {
+            //   $form->addElement(form_makeOpenTag('div', array('class'=>'editButtons')));
+            //   $form->addElement(form_makeButton('submit', 'save', $lang['btn_save'], array('id'=>'edbtn__save', 'accesskey'=>'s', 'tabindex'=>'4')));
+            //   $form->addElement(form_makeButton('submit', 'preview', $lang['btn_preview'], array('id'=>'edbtn__preview', 'accesskey'=>'p', 'tabindex'=>'5')));
+            //   $form->addElement(form_makeButton('submit', 'draftdel', $lang['btn_cancel'], array('tabindex'=>'6')));
+            //   $form->addElement(form_makeCloseTag('div'));
+            //   $form->addElement(form_makeOpenTag('div', array('class'=>'summary')));
+            //   $form->addElement(form_makeTextField('summary', $SUM, $lang['summary'], 'edit__summary', 'nowrap', array('size'=>'50', 'tabindex'=>'2')));
+            //   $elem = html_minoredit();
+            //   if ($elem) $form->addElement($elem);
+            //        $form->addElement(form_makeCloseTag('div'));
+          //}
+          
+          $form->addElement(form_makeCloseTag('div'));
+          //if($wr && $conf['license']){
+            //   $form->addElement(form_makeOpenTag('div', array('class'=>'license')));
+            //   $out  = $lang['licenseok'];
+            //   $out .= ' <a href="'.$license[$conf['license']]['url'].'" rel="license" class="urlextern"';
+            //   if($conf['target']['extern']) $out .= ' target="'.$conf['target']['extern'].'"';
+            //   $out .= '>'.$license[$conf['license']]['name'].'</a>';
+            //   $form->addElement($out);
+            //   $form->addElement(form_makeCloseTag('div'));
+          //}
+                                                       //
+          if ($wr) {
+               // sets changed to true when previewe
+               echo '<script type="text/javascript" charset="utf-8"><!--//--><![CDATA[//><!--'. NL;
+               echo 'textChanged = ' . ($mod ? 'true' : 'false');
+               echo '//--><!]]></script>' . NL;
+          } ?>
+          <div style="width:90%;">
+               <div class="toolbar">
+                    <div id="draft__status"><?php if(!empty($INFO['draft'])) echo $lang['draftdate'].' '.dformat();?></div>
+                    <div id="tool__bar"><?php if ($wr && $data['media_manager']){?><a href="<?php echo DOKU_BASE?>lib/exe/mediamanager.php?ns=<?php echo $INFO['namespace']?>" target="_blank"><?php echo $lang['mediaselect'] ?></a><?php }?></div>
+               </div>
+     <?php
+          html_form('edit', $form);
+          print '</div>'.NL;
+
+          return str_replace('</form>','',str_replace('<form id="dw__editform" method="post" action="" accept-charset="utf-8">','',ob_get_clean()));
+         
+     }
+
      /***** Graph API functions *****/
 
      private $graph_url = 'https://graph.pirati.cz/';
